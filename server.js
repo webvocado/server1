f1216d90c8aac4d40310c1e7e355f28c2d05a7ce
SeungHun <bsh8164@gmail.com>
2023-12-13 13:44:50 +0900

    get/post

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ObjectId } = require('mongodb')
+
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
-
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) 
 
-
-const { MongoClient } = require('mongodb')
 
 let db
 const url = 'mongodb+srv://admin:qwer1234@webvoca.qv7a8aa.mongodb.net/?retryWrites=true&w=majority';
@@ -22,24 +21,17 @@ new MongoClient(url).connect().then((client)=>{
   console.log(err)
 })
 
+
+
 app.get('/', function(요청, 응답) {
   응답.sendFile(__dirname + '/index.html')
 })
-
-// app.get('/news', (요청, 응답) => {
-//   db.collection('post').insertOne({title : '어쩌구'})
-//   응답.send('오늘 비옴')
-// })
 
 app.get('/list', async (요청, 응답) => {
   let result = await db.collection('post').find().toArray() 
   // 컬렉션의 document 전부 출력하는 법
   응답.render('list.ejs', { posts : result }) 
 }) 
-
-// app.get('/time', (req, res) => {
-//   res.render('time.ejs', { time : new Date() })
-// })
 
 app.get('/write', (req, res) => {
     res.render('write.ejs')
@@ -65,3 +57,24 @@ app.post('/add', async (req, res) => {
   // DB 오류 같은걸로 에러메시지가 뜰 수 있는데 이럴 때 try catch문 사용해보자.
 
 }); 
+
+
+
+// URL 파라미터 이용하기, 링크 누르면 상세페이지로 이동하기
+// new ObjectId(req.params.id) 
+app.get('/detail/:id', async (req, res) => {
+  try {
+    let result = await db.collection('post').findOne({ _id : new ObjectId(req.params.id)})
+    if (result == null) {
+      res.status(400).send('글이 존재하지 않습니다.')  
+    } else {
+      res.render('detail.ejs', { result : result })
+    }
+    
+  } catch {
+    res.send('주소를 잘못 입력했습니다.')
+  }
+  // try catch 문으로 예외처리 (다른 url을 쳤을 때!)
+  // if문 사용하여 너무 긴 문자를 사용했을 때 null이 뜨면 오류 던지기
+});
+// findOne (하나의 document 조회하기)
